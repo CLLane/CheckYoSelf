a7f20deef71d59189b9499081cb1fce90084cc70
Chris Lane <pankokrumbs@gmail.com>
2019-06-04 21:59:29 -0600

    Add syntax to main.js

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ var plusButton = document.querySelector('#plus-button');
 var titleInput = document.querySelector('#title-input');
 var itemInput = document.querySelector('#item-input');
 var makeCardButton = document.querySelector('#make-button');
-var tentativeItemList = document.querySelector('#tentative-item-list')
+var tentativeItemList = document.querySelector('#tentative-item-list');
+var clearItemsButton = document.querySelector('#clear-button');
+var searchBar = document.querySelector('#search-input');
+var filterUrgentButton = document.querySelector('#filter-button');
 var cardsArray = [];
-var clearItemsButton = document.querySelector('#clear-button')
-var searchBar = document.querySelector('#search-input')
-var filterUrgentButton = document.querySelector('#filter-button')
 
 pageloadHandler();
 
@@ -19,14 +19,7 @@ titleInput.addEventListener('keyup', makeCardButtonHelper);
 tentativeItemList.addEventListener('click', deleteTentativeItem);
 clearItemsButton.addEventListener('click', clearButtonHandler);
 searchBar.addEventListener('keyup', searchFunction);
-filterUrgentButton.addEventListener('click', filterUrgentHandler)
-
-
-function populateCards(array) {
-  for (var i = 0; i < array.length; i++) {
-    generateCard(array[i]);
-  }
-}
+filterUrgentButton.addEventListener('click', filterUrgentHandler);
 
 function clearButtonHandler(e) {
   e.preventDefault();
@@ -44,7 +37,7 @@ function tentativeItemHandler(e) {
   resetItemInput();
   enableMakeButtonUl();
   disablePlusButton();
-} 
+}
 
 function makeCardButtonHelper(e) {
  enableMakeButtonUl();
@@ -59,10 +52,10 @@ function makeCard(e) {
 }
 
 function pageloadHandler() {
-    cardsArray = JSON.parse(localStorage.getItem('todoListArray')) || [];
-    instantiateCards();
-    populateCards(cardsArray);
-    noTaskPrompt();
+  cardsArray = JSON.parse(localStorage.getItem('todoListArray')) || [];
+  instantiateCards();
+  populateCards(cardsArray);
+  noTaskPrompt();
 }
 
 function cardSectionHandler(e) {
@@ -70,6 +63,26 @@ function cardSectionHandler(e) {
   urgentToggle(e);
   checkTasks(e);
   noTaskPrompt();
+}
+
+function filterUrgentHandler() {
+  cardSection.innerHTML = '';
+  urgentButtonStyle();
+  urgentCardsArray();
+  urgentPrompt();
+}
+
+function disableButtonHelper (e) {
+  e.preventDefault();
+  disablePlusButton();
+  disableClearAll();
+}
+
+
+function populateCards(array) {
+  for (var i = 0; i < array.length; i++) {
+  generateCard(array[i]);
+  }
 }
 
 function disableMakeButton() {
@@ -93,9 +106,11 @@ function enableMakeButtonUl () {
 function resetItemInput() {
   itemInput.value = '';
 }
+
 function resetTitleInput() {
   titleInput.value = '';
 }
+
 function clearTaskUl() {
   tentativeItemList.innerHTML = '';
 }
@@ -114,21 +129,21 @@ function cardInstance(e) {
     createTodoTask();
     localStorage.removeItem('todoTasks');
   }
-  generateCard(taskCard)
+  generateCard(taskCard);
 }
 
 function createToDoList() {
   var tasksArray = JSON.parse(localStorage.getItem('todoListArray'));
   var newList = new ToDoList(Date.now(), titleInput.value, tasksArray);
-  cardsArray.push(newList)
-  newList.saveToStorage()
+  cardsArray.push(newList);
+  newList.saveToStorage();
   generateCard(newList);
 }
 
 function createTentativeItem(input, id) {
   var tentativeItem = `<li class="tentative-item" id="tentative-item" data-id="${id}"> 
     <img src="images/delete.svg" class="delete-task"> ${input} </li>`
-  tentativeItemList.insertAdjacentHTML('beforeend', tentativeItem)
+  tentativeItemList.insertAdjacentHTML('beforeend', tentativeItem);
 }
 
 function deleteTentativeItem(e) {
@@ -143,8 +158,8 @@ function deleteTentativeItem(e) {
 
 function deleteTodoCard(e) {
   if (e.target.classList.contains('delete-card')) {
-    var index = getCardById(e, cardsArray)
-    cardsArray[index].deleteFromStorage(index)
+    var index = getCardById(e, cardsArray);
+    cardsArray[index].deleteFromStorage(index);
     e.target.closest('.todo-card').remove();
   }
   noTaskPrompt();
@@ -156,12 +171,6 @@ function getCardById(e, array) {
     return arrayObj.id === parseInt(cardId);
 });
   return arrayIndex;
-};
-
-function disableButtonHelper (e) {
-  e.preventDefault();
-  disablePlusButton();
-  disableClearAll();
 }
 
 function disablePlusButton() {
@@ -177,18 +186,18 @@ function addItemToArray() {
   }
   todoTasks.push(newTask);
   localStorage.setItem('todoTasks', JSON.stringify(todoTasks));
-  return newTask.id;
+    return newTask.id;
 }
 
 function deleteItemFromArray(e) {
-  if (e.target.className === 'delete-task'){
+  if (e.target.className === 'delete-task') {
     var todoTasks = JSON.parse(localStorage.getItem('todoTasks'));
     var index = getTaskById(e, todoTasks);
-    todoTasks.splice(index, 1)
-    reinstantiateTodo(e).saveToStorage(todoTasks)
+    todoTasks.splice(index, 1);
+    reinstantiateTodo(e).saveToStorage(todoTasks);
     e.target.closest('.tentative-item').remove();
   }
-}
+};
 
 function getTaskById(e, array) {
   var taskId = e.target.closest('.tentative-item').getAttribute('data-id');
@@ -200,39 +209,37 @@ function getTaskById(e, array) {
 
 function reinstantiateTodo(e) {
   var todoInstance = new TodoItems(e.target.closest('li').innerText, e.target.getAttribute('data-id'));
-  return todoInstance
+  return todoInstance;
 }
 
 function instantiateCards() {
   if (cardsArray.length > 0) {
     var reinstantiatedArray = JSON.parse(localStorage.getItem('todoListArray')).map(function(listObject){
-      return new ToDoList(listObject.id, listObject.title, listObject.tasksArray, listObject.urgent)
+        return new ToDoList(listObject.id, listObject.title, listObject.tasksArray, listObject.urgent);
     })
-    cardsArray = reinstantiatedArray
+  cardsArray = reinstantiatedArray;
   }
 }
 
 function noTaskPrompt() {
-var prompt = document.querySelector('#no-task')
-if(cardSection.contains(prompt) && cardsArray.length > 0) {
-  cardSection.removeChild(prompt);
+  var prompt = document.querySelector('#no-task')
+  if (cardSection.contains(prompt) && cardsArray.length > 0) {
+    cardSection.removeChild(prompt);
 }
-if(prompt === undefined && cardsArray < 1) {
-  var taskPrompt = `<p class="no-task hidden" id="no-task">No Currently Open Task</p>`
-  cardSection.insertAdjacentHTML('afterbegin', taskPrompt);
+  if (prompt === undefined && cardsArray < 1) {
+    var taskPrompt = `<p class="no-task hidden" id="no-task">No Currently Open Task</p>`
+    cardSection.insertAdjacentHTML('afterbegin', taskPrompt);
   }
 }
 
   function createTaskList(array) {
     var listItems = ``;
-    for(var i = 0; i < array.length; i++) {
-    var checked = checkedOrNot(array[i]);
-      listItems += `<li data-id="${array[i].id}" class="${checked}"><img src="images/checkbox.svg" class="unchecked">
-        ${array[i].text}
-      </li>`
+    for (var i = 0; i < array.length; i++) {
+      var checked = checkedOrNot(array[i]);
+      listItems += `<li data-id="${array[i].id}" class="${checked}"><img src="images/checkbox.svg" class="unchecked">${array[i].text}</li>`
     }
     return listItems;
-  };
+  }
 
 function generateCard(newTodoCard) {
   var cardItems = createTaskList(newTodoCard.tasksArray);
@@ -240,60 +247,58 @@ function generateCard(newTodoCard) {
   var urgent = urgentCheck(newTodoCard);
   var taskCard = `<article class="todo-card ${urgent}" id="todo-card" data-id="${newTodoCard.id}">
     <h2>${newTodoCard.title}</h2>
-        <ul class="on-card-tasks on-card-tasks-${newTodoCard.id}">
-        ${cardItems}
-        </ul>
-        <footer>
-        <div class="card-image">
-        <img src="images/urgent.svg" class="urgent-card">
-        <p>URGENT</p>
-        </div>
-        <div class="card-image">
-        <img src="images/delete.svg" class="delete-card" disable>
-        <p>DELETE</p>
-        </div>
-        </footer>
-        </article>`
-        cardSection.insertAdjacentHTML('afterbegin', taskCard);
-
-};
+      <ul class="on-card-tasks on-card-tasks-${newTodoCard.id}">
+      ${cardItems}
+      </ul>
+      <footer>
+      <div class="card-image">
+      <img src="images/urgent.svg" class="urgent-card">
+      <p>URGENT</p>
+      </div>
+      <div class="card-image">
+      <img src="images/delete.svg" class="delete-card" disable>
+      <p>DELETE</p>
+      </div>
+      </footer>
+      </article>`
+  cardSection.insertAdjacentHTML('afterbegin', taskCard);
+}
 
 
 function urgentToggle(e) {
   if (e.target.classList.contains('urgent-card')) {
     urgentCardChange(e);
-    var index = getCardById(e, cardsArray)
+    var index = getCardById(e, cardsArray);
     cardsArray[index].updateToDo(cardsArray);
     instantiateCards();
   }
 }
 
 function urgentCardChange(e) {
-  e.target.closest('article').classList.toggle('urgent')
+  e.target.closest('article').classList.toggle('urgent');
 }
 
 function urgentCheck(newTodoCard) {
   if (newTodoCard.urgent) {
-    return 'urgent'
+    return 'urgent';
   }
 }
 
-
 function checkTasks(e) {
-  if(e.target.classList.contains('unchecked')) {
+  if (e.target.classList.contains('unchecked')) {
     checkTasksChange(e);
     checkedCheck(e);
   }
 }
 
 function checkTasksChange(e) {
-  e.target.closest('li').classList.toggle('checked')
+  e.target.closest('li').classList.toggle('checked');
 }
 
 function checkedCheck(e) {
  var index = getCardById(e, cardsArray);
- var taskObj = cardsArray[index].tasksArray
- var taskId = e.target.closest('li').getAttribute('data-id')
+ var taskObj = cardsArray[index].tasksArray;
+ var taskId = e.target.closest('li').getAttribute('data-id');
  var taskIndex = taskObj.findIndex(function(itemObj){
   return itemObj.id == parseInt(taskId);
  });
@@ -306,7 +311,6 @@ function checkedOrNot(array) {
   }
 }
 
-
 function enableDeleteButton(e) {
   var index = getCardById(e, cardsArray);
   var deleteObj = cardsArray[index].tasksArray;
@@ -318,9 +322,6 @@ function enableDeleteButton(e) {
   } 
 }
 
-// ------Search Function----------\\
-
-
 function searchFunction(arrayName) {
   if (searchBar.value === '' || null) {
     emptySearchPopulation();
@@ -328,33 +329,23 @@ function searchFunction(arrayName) {
   if (filterUrgentButton.clicked === true) {
     cardSection.innerHTML = '';
     populateCards(searchArray(urgentArray(), searchBar.value));
-} else {
-  var newArray = searchArray(cardsArray, searchBar.value);
-  cardSection.innerHTML = ''
-  populateCards(newArray);
-}
+  } else {
+    var newArray = searchArray(cardsArray, searchBar.value);
+    cardSection.innerHTML = ''
+    populateCards(newArray);
+  }
 }
 
-function emptySearchPopulation(){
+function emptySearchPopulation() {
   cardSection.innerText = '';
   populateCards(cardsArray);
 }
 
-
-function searchArray(array, searchWords){
+function searchArray(array, searchWords) {
   var searchResultsArray = array.filter(function(arrayObject){
-  return arrayObject.title.includes(searchWords) === true;
+    return arrayObject.title.includes(searchWords) === true;
   });
   return searchResultsArray;
-}
-
-// -----------Filter Urgent----------
-
-function filterUrgentHandler() {
-  cardSection.innerHTML = '';
-  urgentButtonStyle();
-  urgentCardsArray();
-  urgentPrompt();
 }
 
 function urgentButtonStyle() {
@@ -363,12 +354,11 @@ function urgentButtonStyle() {
 }
 
 function urgentCardsArray(){
-    if (filterUrgentButton.clicked === true){
+  if (filterUrgentButton.clicked === true) {
     var urgent = urgentArray();
-    populateCards(urgent)
-  }
-   else {
-    populateCards(cardsArray)
+    populateCards(urgent);
+  } else {
+    populateCards(cardsArray);
   }
 }
 
@@ -382,6 +372,6 @@ function urgentArray(){
 function urgentPrompt() {
   if (filterUrgentButton.clicked === true && urgentArray().length === 0) {
     var urgentPrompt = '<p class="urgent-prompt">No Urgent Tasks</p>';
-    cardSection.insertAdjacentHTML('afterbegin', urgentPrompt)
+    cardSection.insertAdjacentHTML('afterbegin', urgentPrompt);
   }
 }
